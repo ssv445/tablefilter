9c3909dfac967fc23c8a30a18d44007615254fc7
Shyam Sunder Verma <shyam@readybytes.in>
2017-08-11 11:42:57 +0530

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function () { 
     var s = document.createElement('script');
     s.setAttribute("type", "text/javascript");
-    s.setAttribute("src", "https://rawgit.com/Sjord/tablefilter/master/tablefilter.js");
+    s.setAttribute("src", "https://rawgit.com/ssv445/tablefilter/master/tablefilter.js");
     document.body.appendChild(s);
 })();
